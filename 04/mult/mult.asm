fc96502b78de570b44c6e571abbc20841555415e
Kenny Wu <kenny.wu@lendesk.com>
2019-01-09 12:06:52 -0800

    Project 4 - mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// TODO: Fail early on 0 arg values for optimization
+
+// Setup variables
+@counter
+M=0
+
+@sum
+M=0
+
+// Initialize Product as 0
+@R2
+M=0
+
+(LOOP)
+    // Counter has reached RAM[0]
+    @R0
+    D=M
+    @counter
+    D=D-M
+    @END
+    D;JEQ
+
+    // Computation - Add R1 value to sum
+    @R1
+    D=M
+    @sum
+    D=D+M
+    M=D
+
+    // Set Result to RAM[2]
+    @R2
+    M=D
+
+    // Increment Counter
+    @counter
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0; JMP
