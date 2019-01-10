e5cfb603b742235ecac5a9a5d2ead927628b04ad
Kenny Wu <kenny.wu@lendesk.com>
2019-01-09 18:27:18 -0800

    Project 4 - Complete Fill / Include other hacks + outputs

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,92 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Setup variables
+@SCREEN // Screen Base
+D=A
+@addr
+M=D
+
+@KBD // Print Limit
+D=A
+@screen_end
+M=D
+
+@i // Black Counter
+M=0
+
+@j // White Counter
+M=0
+
+(LOOP)
+    @KBD
+    D=M
+
+    @WRITE_BLACK
+    D;JGT
+
+    @WRITE_WHITE
+    D;JEQ
+
+(WRITE_BLACK)
+    @i
+    D=M
+
+    @addr
+    D=M+D
+    @temp_black_address
+    M=D
+
+    // Exit when reached screen end
+    @screen_end
+    D=M-D
+    @LOOP
+    D;JEQ
+
+    // Print
+    @temp_black_address
+    A=M
+    M=-1
+
+    // Increment for next print
+    @i
+    M=M+1
+
+    // Rest White Print Counter
+    @j
+    M=0
+
+    @LOOP
+    0;JMP
+
+(WRITE_WHITE)
+    @j
+    D=M
+
+    @addr
+    D=M+D
+    @temp_white_address
+    M=D
+
+    // Exit when reached screen end
+    @screen_end
+    D=M-D
+    @LOOP
+    D;JEQ
+
+    // Print
+    @temp_white_address
+    A=M
+    M=0
+
+    // Increment for next print
+    @j
+    M=M+1
+    
+    // Rest Black Print Counter
+    @i
+    M=0
+
+    @LOOP
+    0;JMP
